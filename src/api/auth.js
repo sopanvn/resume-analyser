0ed3ccb87884b9eaef8117400b208dfdf89cef2b
Sopan <nadagudesopan@gmail.com>
2026-06-05 17:57:07 +0530

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,13 +1,4 @@
 import axios from "axios";
 
 const API = import.meta.env.VITE_API_URL;
-
-// REGISTER
-export const registerUser = (data) => {
-  return axios.post(`${API}/api/auth/register`, data);
-};
-
-// LOGIN
-export const loginUser = (data) => {
-  return axios.post(`${API}/api/auth/login`, data);
-};
+console.log("API URL:", API);
